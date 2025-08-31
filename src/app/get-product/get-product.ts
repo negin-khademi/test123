2624ab78d19a36cb03b71bc7a22acaabbc019001
negin-khademi <negin.khademi@gmail.com>
2025-08-31 17:25:24 +0330

    feat : send token to server and get res

--- src/app/get-product/get-product.ts
+++ src/app/get-product/get-product.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
 import { Shared } from "../shared";
 
 @Component({
@@ -9,7 +11,36 @@ import { Shared } from "../shared";
 })
 export class GetProduct {
   userData: any;
-  constructor(private auth: Shared) {
+  constructor(
+    private auth: Shared,
+    private router: Router,
+    private http: HttpClient
+  ) {
     this.userData = this.auth.getUserData();
+  }
+  showResult() {
+    const token = this.userData?.model?.token || localStorage.getItem("token");
+
+    if (!token) {
+      console.error("No token found");
+      return;
+    }
+
+    this.http
+      .post(
+        "https://localhost:7097/api/Managment/test",
+        {}, // body can be empty
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
+      .subscribe({
+        next: (res: any) => {
+          console.log("Success: token is valid", res);
+        },
+        error: (err) => {
+          console.error("Error: token invalid or expired", err);
+        },
+      });
   }
 }
