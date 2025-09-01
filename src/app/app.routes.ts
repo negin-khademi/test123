780ee1650507f1cbfc98ff9a901450f7c715a233
negin-khademi <negin.khademi@gmail.com>
2025-09-01 10:47:32 +0330

    feat : add ui route for showing products

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { GetProduct } from "./get-product/get-product";
 import { Login } from "./login/login";
 import { Routes } from "@angular/router";
+import { Ui } from "./ui/ui";
 
 export const routes: Routes = [
   { path: "", component: Login },
   { path: "getProduct", component: GetProduct },
+  { path: "ui", component: Ui },
 ];
